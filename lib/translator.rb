f806483f9feaf6b48dbb1d3e5e5f5c3504b2f30f
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-28 02:20:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,7 @@
 # require modules here
 require 'yaml'
-def load_library("./lib/emoticons.yml")
-  # code goes here
- emoticons = YAML.load_file("./lib/emoticons.yml")
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
   new_hash = {}
   
   emoticons.each do |key,value|
